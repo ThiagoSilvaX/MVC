c0e5bdb385239cd3ef3113298f3f91fd38590a7c
carlos santana <carlos.santana33890@gmail.com>
2022-11-30 11:27:56 -0300

    alteração no package do controller

--- src/main/java/controllers/MetaisController.java
+++ src/main/java/controllers/MetaisController.java
@@ -1,4 +1,4 @@
-package br.fatecrl.mvcdemo.controllers;
+package controllers;
 import br.fatecrl.mvcdemo.models.Metais;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
